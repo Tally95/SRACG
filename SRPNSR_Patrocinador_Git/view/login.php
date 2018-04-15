03cd9d8084806784d2d6bd209d4409f32bdf7058
Marco Garita <marcogaray15@hotmail.com>
2018-04-15 00:14:47 -0600

    Se agrego el login de usuarios a el modulo de patros y ya esta listo! :)

--- SRPNSR_Patrocinador_Git/view/login.php
+++ SRPNSR_Patrocinador_Git/view/login.php
@@ -1,4 +1,31 @@
-<div class="loginPat">
+ <div class="fila_4L">
+    <div class="cajaLoginL">
+        <div class="botonesLoginL">
+            <a href="?c=login&aIndex">
+                <button class="btnL azulOL">Iniciar Sesión</button>
+            </a> 
+        </div>
+
+        <h1 style="color: white;margin: auto; height: 50px; ">Bienvenidos</h1>
+
+        <form action="?c=login&a=ValidarLogin" method="post">
+            <div class="form-group1L">
+                <input class= "inputL" name="email" type="email" required placeholder="Correo Electronico">
+            </div>
+            <div class="form-group1L">
+                <input class= "inputL" name="pass" type="password" required placeholder="Contraseña">
+            </div> 
+            <div class="botonesIngresarL">
+                <button class="btnL azulOL">Ingresar</button>
+            </div>
+        </form>
+        
+    </div>
+</div>
+
+
+
+<!--<div class="loginPat">
     <h1 style="padding-top: 20px;">INICIAR SESIÓN</h1>
     <div class="cajonReg" style="padding-top: 10px;"> 
         <div style="padding: 9px;">
@@ -19,4 +46,6 @@
             <button class="botonesLogin">Cancelar</button>
         </div>
     </div>
-</div>
+</div>-->
+
+
