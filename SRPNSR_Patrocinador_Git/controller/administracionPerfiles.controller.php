eb1c8c1cceac3898705c36f336db5f2f1652c2c4
Marco Garita <marcogaray15@hotmail.com>
2018-04-15 15:35:32 -0600

    Ahora las tres vistas se construyen en el mismo controller

--- SRPNSR_Patrocinador_Git/controller/administracionPerfiles.controller.php
+++ SRPNSR_Patrocinador_Git/controller/administracionPerfiles.controller.php
@@ -5,4 +5,17 @@ class administracionPerfilesController{
         require_once 'view/administracionPerfiles.php';
         require_once 'view/footer.php';
     } 
+    
+    public function IndexAgregar(){
+        require_once 'view/header.php';
+        require_once 'view/agregarPerfiles.php';
+        require_once 'view/footer.php';
+    } 
+    
+    public function IndexEditar(){
+        require_once 'view/header.php';
+        require_once 'view/editarPerfiles.php';
+        require_once 'view/footer.php';
+    } 
+    
 }
